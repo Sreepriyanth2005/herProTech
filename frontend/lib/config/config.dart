843b6b47f8ce13d9bb9249a0930d48609f43bf6e
Sreepriyanth2005 <sreepriyantns.22aid2kongu.edu>
2025-01-23 03:50:09 +0530

    changes in the backend

--- frontend/lib/config/config.dart
+++ frontend/lib/config/config.dart
@@ -1,4 +1,4 @@
 class Config{
-  // static const String baseUrl = "https://herprotech-backend-1.onrender.com";
-  static const String baseUrl = "http://192.168.140.33:3000";
+  static const String baseUrl = "https://herprotech-backend-1.onrender.com";
+  // static const String baseUrl = "http://192.168.140.33:3000";
 }
--- frontend/lib/widgets/sos_button.dart
+++ frontend/lib/widgets/sos_button.dart
@@ -2,9 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:herprotech/assets/style_constants.dart';
 import 'package:herprotech/wrapper/responsive_container.dart';
 import 'package:herprotech/wrapper/responsive_text.dart';
+import 'package:http/http.dart' as http;
 
-class SosButtonWidget extends StatelessWidget {
+class SosButtonWidget extends StatefulWidget {
   const SosButtonWidget({super.key});
+
+  @override
+  State<SosButtonWidget> createState() => _SosButtonWidgetState();
+}
+
+class _SosButtonWidgetState extends State<SosButtonWidget> {
+  Future callWebServer() async {
+    final String url = "https://ful-sos-2.onrender.com";
+    try {
+      await http.get(Uri.parse(url));
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('SOS Alert Sent!'),
+          backgroundColor: Colors.green,
+        ),
+      );
+    } catch (error) {
+      print('Error: $error');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Failed to send SOS Alert!'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,15 +43,16 @@ class SosButtonWidget extends StatelessWidget {
         shape: BoxShape.circle,
       ),
       child: GestureDetector(
-        onTap: () {
+        onTap: () async {
           print('SOS Button Pressed');
+          await callWebServer();
         },
         child: Center(
           child: ResponsiveText(
             'SOS',
             style: StyleConstants.customStyle(
               50,
-              Color.fromRGBO(4, 6, 49, 1),
+              const Color.fromRGBO(4, 6, 49, 1),
               FontWeight.w500,
             ),
           ),
